70ccfe1ef885d3f173593550b7603e58b938f9fc
asandersa <97736449+asandersa@users.noreply.github.com>
2022-05-31 11:43:21 +0300

    JRTB-2: added stub telegram bot (#15)
    * JRTB-2: added stub telegram bot
    * JRTB-2: updated project version and added to RELEASE_NOTES
    * JRTB-2: deleted the test

--- src/test/java/com/github/asandersa/jrtb/JavarushTelegramBotApplicationTests.java
+++ src/test/java/com/github/asandersa/jrtb/JavarushTelegramBotApplicationTests.java
@@ -1,13 +1,27 @@
 package com.github.asandersa.jrtb;
 
+import com.github.asandersa.jrtb.bot.JavarushTelegramBot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 @SpringBootTest
 class JavarushTelegramBotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+//	JavarushTelegramBot underTest = new JavarushTelegramBot();
+//	@Test
+//	void onUpdateReceived() {
+//		Update update = new Update();
+//		Message message = new Message();
+//		Chat chat = new Chat();
+//		chat.setId(1234L);
+//		message.setText("TEXT");
+//		message.setChat(chat);
+//		update.setMessage(message);
+//
+//		underTest.onUpdateReceived(update);
+//	}
 }
