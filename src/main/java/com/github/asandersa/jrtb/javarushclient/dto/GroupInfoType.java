d5864dc96a3b60992d072262e959a5ae635ef39a
Anastasia Prokopchik <anastasia.yu.prokopchik@gmail.com>
2022-06-08 10:49:18 +0300

    JRTB-5: added API interaction, tests

---
+++ src/main/java/com/github/asandersa/jrtb/javarushclient/dto/GroupInfoType.java
@@ -0,0 +1,5 @@
+package com.github.asandersa.jrtb.javarushclient.dto;
+
+public enum GroupInfoType {
+    UNKNOWN, CITY, COMPANY, COLLEGE, TECH, SPECIAL, COUNTRY
+}
---
+++ src/main/java/com/github/asandersa/jrtb/javarushclient/dto/MeGroupInfoStatus.java
@@ -0,0 +1,5 @@
+package com.github.asandersa.jrtb.javarushclient.dto;
+
+public enum MeGroupInfoStatus {
+    UNKNOWN, CANDIDATE, INVITEE, MEMBER, EDITOR, MODERATOR, ADMINISTRATOR, BANNED
+}
